3bbcf6ab5e2ff9cd00ca1f18e81e6147c4fa7a2e
bborgcs <babiborges2007@icloud.com>
2026-04-23 09:32:10 -0300

    Commit Teste

---
+++ contadorTeste/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int valor;
+
+        valor = 1;
+
+        while (valor <= 20) {
+            System.out.println(valor);
+            valor = valor + 1;
+        }
+    }
+}
